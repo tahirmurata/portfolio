9b6dc8c37ff1b783ac5245f15ac49bf8afc5c1b1
pastc <147841049+pastc@users.noreply.github.com>
2024-11-21 10:06:38 +0900

    Enable prefetching for project cards on hover

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: {
+    prefetchAll: true
+  },
   site: "https://muratahir.com",
   integrations: [
     tailwind({
