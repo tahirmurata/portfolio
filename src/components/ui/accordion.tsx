15d8b774c0a57ea3dbf6d6c9bb8b6691d3ae6dfb
Tahir Murata <147841049+tahirmurata@users.noreply.github.com>
2025-01-22 20:43:51 +0900

    Refactor AccordionTrigger layout for improved responsiveness and alignment

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -33,10 +33,10 @@ const AccordionTrigger = React.forwardRef<
       )}
       {...props}
     >
-      <div className="flex flex-row gap-x-5">
-        <span className="sm:group-hover:underline">{children}</span>
+      <div className="flex flex-row gap-y-2 sm:gap-y-0 sm:flex-row gap-x-5">
+        <span className="sm:group-hover:underline text-left inline-flex items-center">{children}</span>
         {tags && tags.length > 0 && (
-          <div className="flex space-x-2">
+          <div className="flex flex-wrap gap-2">
             {tags.sort((a, b) => a.length - b.length).map((tag, index) => (
               <Badge key={index} variant="outline">{tag}</Badge>
             ))}
